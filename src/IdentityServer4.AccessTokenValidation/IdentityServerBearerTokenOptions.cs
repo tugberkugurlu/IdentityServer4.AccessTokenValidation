73575e7c8efa073d310c9c4f5f1836e84e53b82f
Tugberk Ugurlu <tugberk@outlook.com>
2015-11-27 17:31:47 +0000

    WIP: started on IdentityServerBearerTokenValidationMiddleware

---
+++ src/IdentityServer4.AccessTokenValidation/IdentityServerBearerTokenOptions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Authentication;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IdentityServer4.AccessTokenValidation
+{
+    public class IdentityServerBearerTokenOptions : AuthenticationOptions
+    {
+        public IdentityServerBearerTokenOptions()
+        {
+            AdditionalScopes = Enumerable.Empty<string>();
+        }
+
+        public IEnumerable<string> AdditionalScopes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the base address of identity server (required)
+        /// </summary>
+        /// <value>
+        /// The authority.
+        /// </value>
+        public string Authority { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scope name for accessing the introspection endpoint.
+        /// </summary>
+        /// <value>
+        /// The scope name.
+        /// </value>
+        public string ScopeName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scope secret for accessing the introspection endpoint.
+        /// </summary>
+        /// <value>
+        /// The scope secret.
+        /// </value>
+        public string ScopeSecret { get; set; }
+    }
+}
---
+++ src/IdentityServer4.AccessTokenValidation/IdentityServerBearerTokenValidationMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.AccessTokenValidation
+{
+    public class IdentityServerBearerTokenValidationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IdentityServerBearerTokenOptions _options;
+        private readonly ILogger<IdentityServerBearerTokenValidationMiddleware> _logger;
+
+        public IdentityServerBearerTokenValidationMiddleware(RequestDelegate next, IdentityServerBearerTokenOptions options, ILogger<IdentityServerBearerTokenValidationMiddleware> logger)
+        {
+            if (next == null)
+            {
+                throw new ArgumentNullException(nameof(next));
+            }
+
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            _next = next;
+            _options = options;
+            _logger = logger;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            return _next.Invoke(context);
+        }
+    }
+}
